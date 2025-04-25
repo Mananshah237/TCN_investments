474cd00ee5551848f6f381558a91c32015c9183b
Manan Shah <mxs9638@mavs.uta.edu>
2025-04-25 02:00:58 -0500

    header changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'TCN Investments',
+  description: 'Created by Manan Shah',
+  generator: 'Manan Shah',
 }
 
 export default function RootLayout({
